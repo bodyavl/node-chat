dd6afa1fe10c30ec24fcd007339e375a817de818
bodyavl <88792596+bodyavl@users.noreply.github.com>
2023-04-03 16:54:13 +0300

    backng messaging

--- index.js
+++ index.js
@@ -4,27 +4,29 @@ dotenv.config();
 require('./database');
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app)
-const { Server } = require("socket.io");
-const io = new Server(server);
-const path = require('path');
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, "views"));
-
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
-    res.render('home');
-})
+const http = require('http').Server(app);
+const cors = require('cors');
+app.use(cors());
+const io = require('socket.io')(http, {
+    cors: {
+        origin: [process.env.ORIGIN]
+    }
+});
+const Message = require('./database/models/message');
 
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
-  });
+    socket.on('chat message', (message) => {
+        const {text, username} = message;
+        Message.create({text, username});
+        socket.broadcast.emit('receive message', message);
+      });
+});
+app.get('/api/getMessages', (req, res) => {
+    const messages = Message.find({});
+    if(!messages) res.sendStatus(404);
+    res.json(messages);
+})
 
-server.listen(process.env.PORT, () => {
+http.listen(process.env.PORT, () => {
     console.log(`On port ${process.env.PORT}`)
 })
