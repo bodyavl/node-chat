358feeaa475fab4f36761c6f53601882ec66a67f
bodyavl <88792596+bodyavl@users.noreply.github.com>
2023-05-20 09:49:55 +0300

    errors resolved

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ io.on('connection', (socket) => {
     socket.on("private message", ({ content, from, to }) => {
         const sendUserSocket = onlineUsers.get(to);
         if (sendUserSocket) {
-          socket.to(sendUserSocket).emit("receive message", from, { content, updatedAt: Date.now(), fromSelf: false }); 
+          socket.to(sendUserSocket).emit("receive message", from, content, Date.now()); 
         }
     })
     socket.on('disconnect', (reason) => {
